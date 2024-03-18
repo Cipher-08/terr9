8d723f8f27f911242e3322e294801b6276440307
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-18 18:33:28 +0530

    Update provider.tf

--- S3/provider.tf
+++ S3/provider.tf
@@ -7,6 +7,6 @@ terraform {
 }
 provider "aws" {
   region = "ap-northeast-1"
-  access_key = "AKIAZQ3DQ4GWZW45EE6V"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
