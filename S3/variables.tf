da89dff7ee1a3a206aa7a77ac03aa3373f044856
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-19 13:06:42 +0530

    Update variables.tf

--- S3/variables.tf
+++ S3/variables.tf
@@ -7,11 +7,13 @@ variable "aws_region" {
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
+  default = "AKIAZQ3DQ4GWYDIIP5NP"
 }
 
 variable "aws_secret_key" {
   description = "AWS secret key"
   type        = string
+  default = "JxOeUcMX6W9pba2ssEEzs4zrxY68sDuInLTHF6k0"
 }
 
 variable "s3_bucket_name" {
