2302cacc3e150612c8ecfefe8b58c3a68670028b
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-19 10:12:49 +0530

    Update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -1,7 +1,13 @@
-resource "aws_s3_bucket" "terrateam" {
-  bucket = lower("infraSity")
-  acl = "private"
-  versioning {
-    enabled = true
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "terrateam" {
+  ami           = "ami-080e1f13689e07408" 
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terrateam"
   }
 }
