39377fe299b94297b35ee1ed2f1db578382aff5a
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-19 11:00:24 +0530

    Update variables.tf

--- S3/variables.tf
+++ S3/variables.tf
@@ -1,2 +1,9 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        = string
+}
