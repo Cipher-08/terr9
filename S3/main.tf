0b9cb17f2615f378c9c35100e8f3872b9427192b
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-19 10:44:28 +0530

    Update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-resource "aws_instance" "terrateam" {
+resource "aws_instance" "terrateam1" {
   ami           = "ami-080e1f13689e07408" 
   instance_type = "t2.micro"
   tags = {
-    Name = "terrateam"
+    Name = "terrateam1"
   }
 }
